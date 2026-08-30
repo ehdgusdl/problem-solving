2b2fc4a5e465623312cdb2093c60b3ce16eb2912
Donghyun Kim <kim138762@gmail.com>
2026-08-30 16:41:51 +0900

    [level 3] Title: N으로 표현, Time: 6.73 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,29 +1,35 @@
 def solution(N, number):
+    
+    # N = num  리턴 1
     if N == number:
         return 1
+    # 중복 제거 용 Set
+    dp = [set() for _ in range(9)]
     
-    # set 만들기 중복 방지
-    dp = [set() for _ in range (9)]
-    
-    #N을 i번 연속 사용하는 숫자 추가 (5, 55, 555)
+    # N을 i번 써서 만들수 있는 모든 경우의 수
     for i in range(1, 9):
         dp[i].add(int(str(N)*i))
         
-        # 다음단계 사칙 연산 계산
         for j in range(1, i):
+
+            # 최대 i번 계산
             for op1 in dp[j]:
                 for op2 in dp[i-j]:
+                    
+                    # 사칙연산 한번씩
                     dp[i].add(op1 + op2)
                     dp[i].add(op1 - op2)
                     dp[i].add(op1 * op2)
-
-                    if op2 !=0:
+                    
+                    # 나누기 조건
+                    if op2 != 0:
                         dp[i].add(op1 // op2)
         
+        # i번 했을때 만들어 졌는지 확인
         if number in dp[i]:
             return i
         
-    #8번 안에 없을때
+    # 8번 안에 안되면 -1
     return -1
     
     
